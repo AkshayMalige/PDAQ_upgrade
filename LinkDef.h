7bb0ed844e3862c34a681074c4c6244273f5d5bb
Akshay <mailme.akshaym@gmail.com>
2018-11-18 11:35:58 +0100

    Raw decoder with main in it

---
+++ LinkDef.h
@@ -0,0 +1,24 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ class SttHit+;
+#pragma link C++ class SttEvent+;
+#pragma link C++ class SttRawHit+;
+
+#pragma link C++ class EmcHit+;
+#pragma link C++ class EmcEvent+;
+
+
+#pragma link C++ class PandaSubsystem+;
+#pragma link C++ class PandaSubsystemEMC+;
+#pragma link C++ class PandaSubsystemSTT+;
+#pragma link C++ class PandaSubsystemSB+;
+//#pragma link C++ class Unpacker+;
+
+
+#endif
