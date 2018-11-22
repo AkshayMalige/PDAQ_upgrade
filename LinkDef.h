ab4daf2c4bc7f1dfc36382059dae531b0a0a3dc7
Akshay <mailme.akshaym@gmail.com>
2018-11-22 09:52:53 +0100

    Working unpacker calibrator and clusterfinder

--- LinkDef.h
+++ LinkDef.h
@@ -9,17 +9,20 @@
 #pragma link C++ class SttRawHit+;
 #pragma link C++ class SttHit+;
 #pragma link C++ class SttEvent+;
-
 #pragma link C++ class EmcHit+;
 #pragma link C++ class EmcEvent+;
 
 #pragma link C++ class Stt_Cal_Event+;
 #pragma link C++ class PandaSttCal+;
 
+#pragma link C++ class SttTrackHit+;
+#pragma link C++ class Stt_Track_Event+;
+
 #pragma link C++ class PandaSubsystem+;
 #pragma link C++ class PandaSubsystemEMC+;
 #pragma link C++ class PandaSubsystemSTT+;
 #pragma link C++ class PandaSubsystemSB+;
+#pragma link C++ class PandaSttTrack+;
 
 
 #endif
